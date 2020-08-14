7abc34eb4d17051197b3ae971cca698f8c174308
ScottLightfoot <gslightfoot@gmail.com>
2020-08-14 12:06:56 -0500

    names with < 2sec runtime

--- names/names.py
+++ names/names.py
@@ -1,3 +1,4 @@
+from os import name
 import time
 
 start_time = time.time()
@@ -20,9 +21,31 @@ for name_1 in names_1:
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print (f"runtime for O(n^c): {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+start_time2 = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+# Replace the nested for loops below with your improvements
+duplicates2 = list(set(names_1)) + list(set(names_2))
+unq = set(duplicates2)
+
+for i in unq:
+    duplicates2.remove(i)
+
+end_time2 = time.time()
+
+print (f"{len(duplicates2)} duplicates:\n\n{', '.join(duplicates2)}\n\n")
+print (f"runtime for O(n): {end_time2 - start_time2} seconds")

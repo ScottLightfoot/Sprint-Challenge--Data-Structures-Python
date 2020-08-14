5caae4ccbd1ee0bf07cfe0da05ba5c14a562a041
ScottLightfoot <gslightfoot@gmail.com>
2020-08-14 10:35:53 -0500

    ring_buff complete, all tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.buffer = []
+        self.idx = 0
 
     def append(self, item):
-        pass
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(item)
+        else:
+            self.buffer.pop(self.idx)
+            self.buffer.insert(self.idx, item)
+            self.idx += 1
+            if self.idx == self.capacity:
+                self.idx = 0
 
     def get(self):
-        pass
+        return self.buffer
